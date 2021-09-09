fe296520aa99db656f153ab6a6290c91b2c82539
mph7 <85843263+mph7@users.noreply.github.com>
2021-09-09 17:40:17 -0300

    Update contador.py

--- contador.py
+++ contador.py
@@ -1,7 +1,5 @@
 import sys
-from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
-    sc = SparkContext("local", "PySpark - Contagem de Palavras")
     words = sc.textFile("/FileStore/tables/livro.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b).sortBy(lambda a:a[1], ascending=False)
     wordCounts.saveAsTextFile("/FileStore/tables/livroWords.txt")
